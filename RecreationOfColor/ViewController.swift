1d0a4c8c884604e6b1613fc1130e1d77030bce3c
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-10 12:15:32 -0600

    added code to my color changing app

--- RecreationOfColor/ViewController.swift
+++ RecreationOfColor/ViewController.swift
@@ -28,8 +28,8 @@ class ViewController: UIViewController
     private func createRandomColor() -> UIColor
     {
         let newColor :UIColor
-        let redValue :CGFloat = CGFoat (Double (arc4random_uniform(256)) / 255.00)
-        let greenValue :CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
         let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
         newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
         
