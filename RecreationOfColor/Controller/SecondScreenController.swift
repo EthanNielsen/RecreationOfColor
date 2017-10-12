e7e1678c417a816c9dead70671ea9c4663869951
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-12 13:11:06 -0600

    created a new screen to my storyboard and a controller for it.

---
+++ RecreationOfColor/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  RecreationOfColor
+//
+//  Created by Nielsen, Ethan on 10/12/17.
+//  Copyright © 2017 Ctech. All rights reserved.
+//
+
+import UIKit
+
+public class SecondScreenController : UIViewController
+{
+    
+}
